75a4ce46f63192ad3eeb730fac1e3b015432e2b9
Iris-Hue <78930747+Iris-Hue@users.noreply.github.com>
2021-06-15 15:37:39 -0700

    add messaging section

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,9 @@
 const alertBanner =document.getElementById("alert");
 
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
 alertBanner.innerHTML =
 `
 <div class= "alert-banner">
@@ -13,5 +17,17 @@ alertBanner.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")){
         alertBanner.style.display = "none"
+    }
+});
+send.addEventListener('click', () => {
+    // ensure user and message fields are filled out
+    if (user.value === "" && message.value === "") {
+        alert("Please fill out user and message fields before sending");
+    } else if (user.value === "") {
+        alert("Please fill out user field before sending");
+    } else if (message.value === "") {
+        alert("Please fill out user field before sending");
+    } else {
+        alert(`Message successfully sent to: ${user.value}`);
     }
 });
