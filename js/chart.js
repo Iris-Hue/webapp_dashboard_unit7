6689d261d2ac3def7c3a249dbd5ea5fd44f0295e
Iris-Hue <78930747+Iris-Hue@users.noreply.github.com>
2021-07-07 09:06:42 -0700

    add charts in JS

--- js/chart.js
+++ js/chart.js
@@ -3,8 +3,13 @@ let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-        backgroundColor: 'rgba(116, 119, 191, 3)',
-        borderWidth: 1
+        borderColor: 'rgb(153 156 224)',
+        borderWidth: 2,
+        lineTension: 0.3,
+        fill: {
+            target: 'origin',
+            above: 'rgb(116 119 190 / 53%)'
+        }
     }]
 };
 let trafficOptions = {
--- js/dashboard.js
+++ js/dashboard.js
@@ -4,19 +4,27 @@ const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
 const send = document.getElementById("send");
 
+alertBanner.innerHTML =
+`
+<div class="alert-banner">
+    <p><strong> Alert: </strong> You have <strong> 4 </strong> overdue tasks to complete</p>
+</div>
+<div>
+    <p class="alert-banner-close">&times;</p>
+</div> ` ;
+
 alertBanner.addEventListener('click', e => {
     const element = e.target;
-    if (element.classList.contains("alert-banner-close")){
-        alertBanner.style.display = "none"
-    }
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none" }
 });
 send.addEventListener('click', () => {
     // ensure user and message fields are filled out
     if (user.value === "" && message.value === "") {
         alert("Please fill out user and message fields before sending");
-    } else if (user.value === "") {
+    } else if (user.value === "" ) {
         alert("Please fill out user field before sending");
-    } else if (message.value === "") {
+    } else if (message.value === "" ) {
         alert("Please fill out user field before sending");
     } else {
         alert(`Message successfully sent to: ${user.value}`);
